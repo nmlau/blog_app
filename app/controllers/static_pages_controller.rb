92a691d0415f5773cc2d325dbe553bd854905674
Nicholas Lau <nicholas.m.lau@gmail.com>
2014-04-13 20:21:24 -0700

    static pages test done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,13 @@
 class StaticPagesController < ApplicationController
   def blog
-    @feed_items = Post.all.where("private = ?", false).paginate(page: params[:page])
-    @recent_feed_items = Post.where("private = ?", false).find(:all, :limit => 5)
+    @feed_items = Post.where("private = ?", false).paginate(page: params[:page])
+    #@recent_feed_items = Post.where("private = ?", false).find(:all, :limit => 5)
+    #@recent_feed_items = Post.where("private = ?", false).all(:limit => 5)
+    @recent_feed_items = Post.where("private = ?", false).limit(5)
   end
 
   def archive
-    @posts = Post.all.where("private = ?", false)
+    @posts = Post.where("private = ?", false)
   end
 
   def about
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,64 +1,42 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe "Static pages" do
+describe "Static pages" do
 
-#   subject { page }
+  subject { page }
 
-#   let(:heading)    { 'Upload App' }
-#   shared_examples_for "all static pages" do
-#     it { should have_selector('h1', text: heading) }
-#     #it { should have_title(heading) }
-#   end
+  before { visit root_path }
 
-#   describe "Home page" do
-#     before { visit root_path }
+  it { should have_title('Nicks Dev Spot') }
+  it { should have_content('Copyright') }
+  it { should have_content('Contact') }
 
-#     it_should_behave_like "all static pages"
-#   end
+  describe "Home/BLog page" do
+    before { visit root_path }
 
-#   describe "Help page" do
-#     before { visit help_path }
-#     let(:heading)     {'Help'}
+    it { should have_content('Recent Posts')}
+    it { should have_content('Projects')}
+  end
 
-#     it { should have_content('Help') }
-#     it_should_behave_like "all static pages"
-#   end
+  describe "About page" do
+    before { visit about_path }
 
-#   describe "About page" do
-#     before { visit about_path }
-#     let(:heading)     {'About Us'}
+    it { should have_content('About Me') }
+    # it { should have_css('img', text: "headshot.ico") }
+  end
 
-#     it { should have_content('About Us') }
-#     it_should_behave_like "all static pages"
-#   end
+  describe "archive" do
+    before { visit archive_path }
 
-#   describe "Contact page" do
-#     before { visit contact_path }
-#     let(:heading)     {'Contact'}
+    it { should have_content('Archive') }
+  end
 
-#     it { should have_content('Contact') }
-#     it_should_behave_like "all static pages"
-#   end
-
-#   describe "signin page" do
-#     before { visit new_user_session_path }
-
-#     it { should have_content('Sign in') }
-#     it { should have_selector('h2', "text: Sign in") }
-#   end
-
-#   it "should have the right links on the layout" do
-#     visit root_path
-#     click_link "About"
-#     expect(page).to have_selector('h1', text: "About Us")
-#     click_link "Help"
-#     expect(page).to have_selector('h1', text: "Help")
-#     click_link "Contact"
-#     expect(page).to have_selector('h1', text: "Contact")
-#     click_link "Home"
-#     click_link "Sign up now!"
-#     expect(page).to have_selector('h2', text: "Sign up")
-#     click_link "nicks upload app"
-#     expect(page).to have_selector('h1', text: "Welcome")
-#   end
-# end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_selector('h1', text: "About Me")
+    click_link "Archive"
+    expect(page).to have_selector('h1', text: "Archive")
+    click_link "Nicks Dev Spot"
+    expect(page).to have_content('Recent Posts')
+  end
+end
